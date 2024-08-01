8f38f5d2af55b2f6af38d1838f2e8c57ab25c594
kumikuikui <ibad.iskhand@gmail.com>
2024-08-01 15:43:42 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FretboardComponent } from "./component/fretboard/fretboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FretboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/component/fretboard/fretboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fretboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './fretboard.component.html',
+  styleUrl: './fretboard.component.scss'
+})
+export class FretboardComponent {
+
+}
