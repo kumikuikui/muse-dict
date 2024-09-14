1f8fa92b53934de58618404fada0ea7e04e18aad
TMS-ibadur <ibadur.iskhandar@tmsasia.com>
2024-09-14 23:18:41 +0800

    working fret

--- src/app/component/fretboard/fretboard.component.ts
+++ src/app/component/fretboard/fretboard.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-fretboard',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './fretboard.component.html',
   styleUrl: './fretboard.component.scss'
 })
 export class FretboardComponent {
+  rows = Array.from({ length: 6 });  // 6 strings
+  columns = Array.from({ length: 24 });  // 12 frets
+
+  // 2D array to store the state of each note
+  noteStates: boolean[][] = Array.from({ length: 6 }, () => Array(12).fill(false));
+
+  isActive(rowIndex: number, colIndex: number): boolean {
+    return this.noteStates[rowIndex][colIndex];
+  }
+
+  toggleNoteState(rowIndex: number, colIndex: number): void {
+    this.noteStates[rowIndex][colIndex] = !this.noteStates[rowIndex][colIndex];
+  }
 
 }
